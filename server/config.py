4fd381171817222d755e4d0be7803a0f90242212
Filippo Sanzani <filippo.sanzani2001@gmail.com>
2022-05-11 12:03:21 +0200

    Revert "server config updated."
    This reverts commit 5924426f5375c62e89ce778cc80ab5659c823765.

--- server/config.py
+++ server/config.py
@@ -1,7 +1,5 @@
 CONFIG = {
     'file_path': '/tmp/cs118/',
     'max_packet_size': 2**16,
-    'max_block_size': 2**15,
-    'connection_timeout': 2,
-    'max_tries': 5
+    'max_block_size': 2**15
 }
