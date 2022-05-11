fb597197affd878a348a05216ef8c2e67cdad8dc
Filippo Sanzani <filippo.sanzani2001@gmail.com>
2022-05-11 17:34:59 +0200

    Close socket on CTRL+C on the server.

--- server/server.py
+++ server/server.py
@@ -1,3 +1,4 @@
+import signal
 from connection_handler import ConnectionHandler
 from config import CONFIG
 import socket as skt
@@ -6,9 +7,13 @@ class Server:
     def __init__(self, address, port) -> None:
         self.socket = skt.socket(skt.AF_INET, skt.SOCK_DGRAM)
         self.socket.bind((address, port))
+        signal.signal(signal.SIGINT, self.close)
+
+    def close(self, signum, frame):
+        self.socket.close()
+        exit(0)
 
     def start(self):
         while True:
             raw_data, address = self.socket.recvfrom(CONFIG["max_packet_size"])
             ConnectionHandler(address, raw_data).start()
-            #TODO: close socket on exit
